d7c12b031fce4e108a905ff4f90194ced83b0e00
younan <okokoknow@naver.com>
2020-08-22 22:38:38 +0900

    [Spring Boot] 자동 설정 만들기.

---
+++ spring-boot-getting-started/src/main/java/me/whiteship/Application.java
@@ -0,0 +1,14 @@
+package me.whiteship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);
+    }
+}
---
+++ spring-boot-getting-started/src/main/java/me/whiteship/HolomanRunner.java
@@ -0,0 +1,18 @@
+package me.whiteship;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HolomanRunner implements ApplicationRunner {
+
+    @Autowired
+    Holoman holoman;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(holoman);
+    }
+}
