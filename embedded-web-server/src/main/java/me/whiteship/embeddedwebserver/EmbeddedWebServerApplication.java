3cb385d5dfd2396312e05df6a8117014cc891936
younan <okokoknow@naver.com>
2020-08-23 18:28:40 +0900

    [Spring Boot] 내장 웹 서버 이해.  - 직접 코드를 통해 Tomcat을 띄워보고,  - Spring Boot에서 AutoConfiguration을 통해 자동으로 Tomcat이라는 서블릿 컨테이너를 생성 / 커스터마이징하고, DispatcherServlet을 등록하는 과정에 대한 학습.

---
+++ embedded-web-server/src/main/java/me/whiteship/embeddedwebserver/EmbeddedWebServerApplication.java
@@ -0,0 +1,42 @@
+package me.whiteship.embeddedwebserver;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class EmbeddedWebServerApplication {
+
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        Context context = tomcat.addContext("/", "/");
+
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
+                writer.println("<html><head><title>");
+                writer.println("Hey, Test");
+                writer.println("</title></head>");
+                writer.println("<body><h1>Hello, Human</h1></body>");
+                writer.println("</html>");
+            }
+        };
+
+        String servletName = "helloServlet";
+        tomcat.addServlet("/", servletName, servlet);
+        context.addServletMappingDecoded("/hello", servletName);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+
+}
---
+++ embedded-web-server/src/test/java/me/whiteship/embeddedwebserver/EmbeddedWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.embeddedwebserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbeddedWebServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
